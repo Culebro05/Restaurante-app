60f9e1f840197f4958626d206eb2d17228e1d11e
Culebro05 <05dianacruz@gmail.com>
2022-03-31 17:28:17 -0600

    LOGIN

--- app/Navigations/AccountsStack.js
+++ app/Navigations/AccountsStack.js
@@ -1,16 +1,28 @@
 import React from 'react'
 import { createStackNavigator } from '@react-navigation/stack'
 import Accounts from '../screens/Account/Accounts'
+import Login from '../screens/Account/Login'
+import Register from '../screens/Account/Register'
 
 const Stack = createStackNavigator()
 
-export default function RestaurantsStack() {
+export default function AccountsStack() {
     return (
        <Stack.Navigator>
             <Stack.Screen
-                name='accounts'
+                name='account'
                 component={Accounts}
-                options={{ title: 'Cuenta'}}
+                options={{ title: 'Mi cuenta'}}
+            />
+            <Stack.Screen
+                name='login'
+                component={Login}
+                options={{title:'Inicie Sesión'}}
+            />
+            <Stack.Screen
+                name='register'
+                component={Register}
+                options={{title:'Registro'}}
             />
        </Stack.Navigator>
     )
--- app/screens/Account/UserGuest.js
+++ app/screens/Account/UserGuest.js
@@ -1,8 +1,12 @@
 import React from 'react'
 import {StyleSheet, View, Text, ScrollView, Image} from 'react-native'
 import { Button } from 'react-native-elements'
+import {useNavigation} from '@react-navigation/native'
+import Login from './Login'
 
 export default function UserGuest(){
+    const navigation = useNavigation()
+
     return(
         <ScrollView style={StyleSheet.container}>
             <Image
@@ -10,6 +14,17 @@ export default function UserGuest(){
                 source={require('../../../assets/img/guest.png')}
             />
             <Text style={styles.title}>Ingresa a tu perfil</Text>
+            <Text style={styles.description}>
+                Busca y visualiza los mejores restaurantes de tu ciudad, no olvides rankear tus favoritos
+                </Text>
+            <View style={styles.viewBtn}>
+                <Button
+                title='Ver tu perfil'
+                buttonStyle={styles.btnStyle}
+                containerStyle={styles.btnContainer}
+                onPress={()=>navigation.navigate('login')}
+                />
+            </View>
         </ScrollView>
     )
 }
@@ -29,6 +44,19 @@ const styles = StyleSheet.create({
         fontSize: 19,
         marginBottom: 10,
         textAlign: 'center'
+    },
+    description:{
+        marginBottom: 20,
+        textAlign: 'center'
+    },
+    viewBtn:{
+        flex: 1,
+        alignItems: 'center'
+    },
+    btnStyle:{
+        backgroudColor: '#00a680'
+    },
+    btnContainer:{
+        width: '70%'
     }
-
 })
