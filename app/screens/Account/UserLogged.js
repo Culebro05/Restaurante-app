6d789bf30d40aeef37862d7d075d2ab810817b87
Culebro05 <05dianacruz@gmail.com>
2022-04-05 17:47:38 -0500

    Registro en firebase

--- app/screens/Account/UserLogged.js
+++ app/screens/Account/UserLogged.js
@@ -1,10 +1,13 @@
 import React from 'react'
 import {View, Text} from 'react-native'
+import firebase from 'firebase'
+import { Button } from 'react-native-elements'
 
 export default function UserLogged(){
     return(
        <View>
          <Text>UserLogged</Text>
+        <Button title='Cerrar sesión' onPress={()=>firebase.auth().signOut}/>
        </View>
     )
 }
