9f6261dc4a6b05027b1a5bb8e9187b2e8b3affba
Culebro05 <05dianacruz@gmail.com>
2022-04-06 12:19:34 -0500

    a

--- app/screens/Account/UserGuest.js
+++ app/screens/Account/UserGuest.js
@@ -2,6 +2,7 @@ import React from 'react'
 import {StyleSheet, View, Text, ScrollView, Image} from 'react-native'
 import { Button } from 'react-native-elements'
 import {useNavigation} from '@react-navigation/native'
+import Login from './Login'
 
 
 export default function UserGuest(){
