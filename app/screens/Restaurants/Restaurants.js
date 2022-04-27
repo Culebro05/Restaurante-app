cc1a2c97e9c120bda9b51fdb0ef3a5fe29bf5a6c
Culebro05 <05dianacruz@gmail.com>
2022-04-27 18:49:43 -0500

    agregar elementos

--- app/screens/Restaurants/Restaurants.js
+++ app/screens/Restaurants/Restaurants.js
@@ -3,7 +3,7 @@ import {StyleSheet, View, Text} from 'react-native'
 import {Icon} from 'react-native-elements'
 import firebase from 'firebase/app'
 
-export default function Restaurants(){
+export default function Restaurants(navigation){
   const {user, setUser} = useState(null)
 
   useEffect( () => {
@@ -22,6 +22,7 @@ export default function Restaurants(){
          name='plus'
          color='#00a680'
          containerStyle={styles.btnContainer}
+         onPress={()=> navigation.navigate('addRestaurants')}
          />
          )}
        </View>
